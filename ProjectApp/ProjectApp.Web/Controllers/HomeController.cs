8382549b5c50e9c4a28a9a6d640db2553556e972
muratmalaev <malaev.m1769@gmail.com>
2023-05-02 21:20:14 +0600

    Testing

--- ProjectApp/ProjectApp.Web/Controllers/HomeController.cs
+++ ProjectApp/ProjectApp.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ProjectApp.Web.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "test" };
         }
 
         // GET api/values/5
